6f1f7403bed8d9f35fb2ac8ed14565397909d44c
Jaskaran Deogan <84167202+jaskarandeogan@users.noreply.github.com>
2023-03-29 22:32:55 -0700

    add sepolia test

---
+++ abi.js
@@ -0,0 +1,28 @@
+const ABI = [
+	{
+		"inputs": [],
+		"name": "getMessages",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_msgs",
+				"type": "string"
+			}
+		],
+		"name": "setMessages",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
---
+++ app.js
@@ -0,0 +1,52 @@
+window.ethereum.enable();
+
+const infuraProjectId = "79cb358f87714576bd9752a049c3332c";
+
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://sepolia.infura.io/v3/${infuraProjectId}`,
+  {
+    name: "sepolia",
+    chainId: 11155111,
+  }
+);
+
+var MessageContract;
+var MessageContractAddress = "0x6F5f4c7D77819fd48F9962dfBad81D2329004fbF";
+var MessageContractABI = ABI;
+var Signer;
+
+async function init() {
+  const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+  const account = accounts[0];
+  const signer = provider.getSigner(account);
+  MessageContract = new ethers.Contract(
+    MessageContractAddress,
+    MessageContractABI,
+    signer
+  );
+}
+
+init();
+
+async function getMessages() {
+  let message = document.getElementById("messagebox");
+  console.log(message);
+  const getMessagesPromise = MessageContract.getMessages();
+  await getMessagesPromise.then((result) => {
+    message.innerHTML = result;
+  });
+}
+
+async function setMessages() {
+  let message = document.getElementById("message").value;
+  console.log(MessageContract);
+  const setMessagesPromise = MessageContract.setMessages(message);
+  await setMessagesPromise.then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+    });
+}
+
+
